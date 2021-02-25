753cadfdacfeb561d2793bb1ae669dcdf5a47ff1
Alexander Milovsky <alex@milovsky.ru>
2021-02-25 15:33:22 +0300

    OT-1887 Refactored detected faces functions

--- keentools_facebuilder/pick_operator.py
+++ keentools_facebuilder/pick_operator.py
@@ -16,8 +16,6 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # ##### END GPL LICENSE BLOCK #####
 
-import contextlib
-import numpy as np
 import logging
 import bpy
 
@@ -28,19 +26,12 @@ from .utils.focal_length import auto_focal_configuration_and_update
 from .utils.manipulate import push_neutral_head_in_undo_history
 
 
-_DETECTED_FACES = None
+_DETECTED_FACES = []
 
 
-def _get_np_image(headnum, camnum):
-    settings = get_main_settings()
-    camera = settings.get_camera(headnum, camnum)
-    if camera is None:
-        return None
-    image = camera.cam_image
-    if not image:
-        return None
-    w, h = image.size[:2]
-    return np.asarray(image.pixels[:]).reshape((h, w, 4))
+def _reset_detected_faces():
+    global _DETECTED_FACES
+    _DETECTED_FACES = []
 
 
 def _get_detected_faces():
@@ -48,26 +39,17 @@ def _get_detected_faces():
     return _DETECTED_FACES
 
 
-def _set_detected_faces(headnum, camnum):
+def _set_detected_faces(faces_info):
     global _DETECTED_FACES
-    settings = get_main_settings()
-    head = settings.get_head(headnum)
-    if head is None:
-        return None
-    img = _get_np_image(headnum, camnum)
-    if img is None:
-        return None
-
-    FBLoader.load_model(headnum)
-    fb = FBLoader.get_builder()
-
-    fb.set_use_emotions(head.should_use_emotions())
-    _DETECTED_FACES = fb.detect_faces(img)
-    return img
+    _DETECTED_FACES = faces_info
+    logger = logging.getLogger(__name__)
+    logger.debug('_DETECTED_FACES: {}'.format(len(_DETECTED_FACES)))
 
 
 def _get_detected_faces_rectangles():
     faces = _get_detected_faces()
+    logger = logging.getLogger(__name__)
+    logger.debug('_get_detected_faces: {}'.format(faces))
     rects = []
     for i, face in enumerate(faces):
         x1, y1 = face.xy_min
@@ -80,49 +62,61 @@ def _get_detected_faces_rectangles():
     return rects
 
 
-def _sort_detected_faces():
-    global _DETECTED_FACES
+def init_detected_faces(fb, headnum, camnum):
+    settings = get_main_settings()
+    head = settings.get_head(headnum)
+    if head is None:
+        return None
+    camera = head.get_camera(camnum)
+    if camera is None:
+        return None
+    img = camera.np_image()
+    if img is None:
+        return None
+
+    fb.set_use_emotions(head.should_use_emotions())
+    _set_detected_faces(fb.detect_faces(img))
+
+    return img
+
+
+def sort_detected_faces():
+    faces = _get_detected_faces()
     rects = _get_detected_faces_rectangles()
+    logger = logging.getLogger(__name__)
+    logger.debug('RECTS BEFORE: {}'.format(rects))
     rects.sort(key=lambda x: x[0])  # order by x1
-    _DETECTED_FACES = [_DETECTED_FACES[x[4]] for x in rects]
+    logger.debug('RECTS AFTER: {}'.format(rects))
+    _set_detected_faces([faces[x[4]] for x in rects])
     return rects
 
 
-def _reset_detected_faces():
-    global _DETECTED_FACES
-    _DETECTED_FACES = None
-
-
-@contextlib.contextmanager
-def _new_preset_pins(fb, kid):
-    fb.remove_pins(kid)
-    yield
-    fb.add_preset_pins(kid)
-    logger = logging.getLogger(__name__)
-    logger.debug('auto_pins_added kid: {}'.format(kid))
-
-
 def _add_pins_to_face(headnum, camnum, rectangle_index):
+    logger = logging.getLogger(__name__)
     fb = FBLoader.get_builder()
     faces = _get_detected_faces()
 
     settings = get_main_settings()
-    kid = settings.get_keyframe(headnum, camnum)
     head = settings.get_head(headnum)
+    kid = settings.get_keyframe(headnum, camnum)
 
     fb.set_use_emotions(head.should_use_emotions())
-
-    with _new_preset_pins(fb, kid):
-        with auto_focal_configuration_and_update(fb, headnum, camnum):
-            fb.detect_face_pose(kid, faces[rectangle_index])
-
-    FBLoader.save_only(headnum)
-    push_neutral_head_in_undo_history(head, kid, 'Add auto-pins')
+    with auto_focal_configuration_and_update(fb, headnum, camnum):
+        result_flag = fb.detect_face_pose(kid, faces[rectangle_index])
+    if result_flag:
+        fb.remove_pins(kid)
+        fb.add_preset_pins(kid)
+        logger.debug('auto_pins_added kid: {}'.format(kid))
+    else:
+        logger.debug('detect_face_pose failed kid: {}'.format(kid))
 
     FBLoader.fb_redraw(headnum, camnum)
     FBLoader.update_pins_count(headnum, camnum)
     FBLoader.update_all_camera_positions(headnum)
     FBLoader.update_all_camera_focals(headnum)
+
+    FBLoader.save_only(headnum)
+    push_neutral_head_in_undo_history(head, kid, 'Add auto-pins')
 
 
 class FB_OT_PickMode(bpy.types.Operator):
@@ -245,7 +239,10 @@ class FB_OT_PickModeStarter(bpy.types.Operator):
             logger.error(message)
             return {'FINISHED'}
 
-        img = _set_detected_faces(self.headnum, self.camnum)
+        FBLoader.load_model(self.headnum)
+        fb = FBLoader.get_builder()
+
+        img = init_detected_faces(fb, self.headnum, self.camnum)
         if img is None:
             message = 'Cannot use camera image'
             self.report({'ERROR'}, message)
@@ -253,7 +250,7 @@ class FB_OT_PickModeStarter(bpy.types.Operator):
             return {'CANCELLED'}
 
         h, w, _ = img.shape
-        rects = _sort_detected_faces()
+        rects = sort_detected_faces()
 
         vp = FBLoader.viewport()
         rectangler = vp.rectangler()
