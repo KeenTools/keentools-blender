9e55220317337759f037e8299352283bb7e7ba28
Alexander Milovsky <alex@milovsky.ru>
2021-12-07 13:45:25 +0300

    Added Blender 3.0 official support

--- keentools_facebuilder/config.py
+++ keentools_facebuilder/config.py
@@ -28,7 +28,7 @@ class Config:
     # Version dependent
     addon_version = '2021.4.0'
     supported_blender_versions = ((2, 80), (2, 81), (2, 82), (2, 83),
-                                  (2, 90), (2, 91), (2, 92), (2, 93))
+                                  (2, 90), (2, 91), (2, 92), (2, 93), (3, 0))
     minimal_blender_api = (2, 80, 60)
 
     # Version independent
