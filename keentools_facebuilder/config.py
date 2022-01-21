c4dc86bd48064ba68369cf9cdc205b5015263028
Alexander Milovsky <alex@milovsky.ru>
2022-01-21 15:17:04 +0300

    OT-2287 Removed unnecessary module usage

--- keentools_facebuilder/config.py
+++ keentools_facebuilder/config.py
@@ -15,11 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # ##### END GPL LICENSE BLOCK #####
-import logging
 import math
 import bpy
-import gpu
-
 
 _company = 'keentools'
 _PT = 'FACEBUILDER_PT_'
