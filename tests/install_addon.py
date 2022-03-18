39e4a74c2e9322ba84e65aca2a13e5f461687a99
Sergey Krivohatskiy <s.krivohatskiy@gmail.com>
2022-03-18 10:56:11 +0300

    Added install addon script for testing (moved from build conf)
    Signed-off-by: Sergey Krivohatskiy <s.krivohatskiy@gmail.com>

---
+++ tests/install_addon.py
@@ -0,0 +1,16 @@
+import sys
+import bpy
+
+assert(sys.argv.index("--") != -1)
+assert(sys.argv.index("--"))
+script_argv = sys.argv[sys.argv.index("--") + 1:]
+assert(len(script_argv) == 2)
+
+addon_path = script_argv[0]
+pkt_path = script_argv[1]
+bpy.ops.preferences.addon_install(filepath=addon_path)
+bpy.ops.preferences.addon_enable(module='keentools_facebuilder')
+bpy.ops.wm.save_userpref()
+
+import keentools.blender_independent_packages.pykeentools_loader as pkt
+pkt.install_core_from_file(pkt_path)
