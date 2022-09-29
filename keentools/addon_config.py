dc78c8dccd9828ff44f49fac85ea53be4256967c
Alexander Milovsky <alex@milovsky.ru>
2022-09-29 12:28:36 +0300

    Version changed to 2022.2.1

--- keentools/addon_config.py
+++ keentools/addon_config.py
@@ -27,7 +27,7 @@ _PT = 'KEENTOOLS_PT_'
 
 class Config:
     # Version dependent
-    addon_version = '2022.2.0'  # (5/5)
+    addon_version = '2022.2.1'  # (5/5)
     supported_blender_versions = ((2, 80), (2, 81), (2, 82), (2, 83),
                                   (2, 90), (2, 91), (2, 92), (2, 93),
                                   (3, 0), (3, 1))
