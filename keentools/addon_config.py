5664ee476bc3a0c8e3a10f950a7c97ae0db5bd4e
Alexander Milovsky <alex@milovsky.ru>
2022-12-09 12:02:02 +0300

    OT-3075 Fix for all 'f' suffices for numbers in shaders

--- keentools/addon_config.py
+++ keentools/addon_config.py
@@ -31,7 +31,7 @@ class Config:
     addon_version = '2022.3.0'  # (5/5)
     supported_blender_versions = ((2, 80), (2, 81), (2, 82), (2, 83),
                                   (2, 90), (2, 91), (2, 92), (2, 93),
-                                  (3, 0), (3, 1), (3, 2), (3, 3))
+                                  (3, 0), (3, 1), (3, 2), (3, 3), (3, 4))
     minimal_blender_api = (2, 80, 60)
 
     fb_tab_category = 'FaceBuilder'
