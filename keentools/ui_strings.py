99e65e11368892969edc6ec54fda35f13156f9c6
Alexander Milovsky <alex@milovsky.ru>
2022-12-13 14:42:40 +0300

    OT-3074 User messages changed

--- keentools/ui_strings.py
+++ keentools/ui_strings.py
@@ -187,13 +187,25 @@ error_messages: Dict = {
         'restart Blender and install the new version of the add-on.'
     ]),
     ErrorType.FBGracePeriod: ErrorMessage(_default_width, [
-        'Your FaceBuilder License is switched to Grace period',
+        'Sorry, there\'s a problem with your FaceBuilder subscription.',
         ' ',
-        'You need to check your license.'
+        'If you\'re using \'Online\' mode, please check that your ',
+        'payment method is still valid and you have sufficient funds on your account.',
+        'We\'ll try to charge the subscription fee again in the next couple of days.',
+        ' ',
+        'If you\'re using \'Offline\' mode and have generated the license file manually,',
+        'it\'s time to renew the file. Please log in to User Portal, ',
+        'download a new file and activate it as you did before.'
     ]),
     ErrorType.GTGracePeriod: ErrorMessage(_default_width, [
-        'Your GeoTracker License is switched to Grace period',
+        'Sorry, there\'s a problem with your GeoTracker subscription.',
         ' ',
-        'You need to check your license.'
+        'If you\'re using \'Online\' mode, please check that your ',
+        'payment method is still valid and you have sufficient funds on your account.',
+        'We\'ll try to charge the subscription fee again in the next couple of days.',
+        ' ',
+        'If you\'re using \'Offline\' mode and have generated the license file manually,',
+        'it\'s time to renew the file. Please log in to User Portal, ',
+        'download a new file and activate it as you did before.'
     ]),
 }
