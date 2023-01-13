2217a198e4887461746dde11cea7c7d750a70eb9
Alexander Milovsky <alex@milovsky.ru>
2023-01-13 15:09:38 +0300

    OT-3129 Fix for revert_default_compositing

--- keentools/utils/compositing.py
+++ keentools/utils/compositing.py
@@ -209,7 +209,8 @@ def revert_default_compositing(scene: Scene) -> bool:
                             node_composite.inputs['Image'])
     else:
         for link in node_tree.links:
-            if link.to_node == node_composite and link.to_socket == node_composite.inputs['Image']:
+            if link.to_node == node_composite \
+                    and link.to_socket == node_composite.inputs['Image']:
                 node_tree.links.remove(link)
-                return False
+        return False
     return True
