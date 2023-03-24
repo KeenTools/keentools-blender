94b439db47a2b25f6da7f69b119f00c009c0eba2
Alexander Milovsky <alex@milovsky.ru>
2023-03-24 16:04:02 +0300

    OT-3452 Renamed Rescale window id. Removed obsolete operator

--- keentools/geotracker_config.py
+++ keentools/geotracker_config.py
@@ -100,13 +100,11 @@ class GTConfig:
     gt_auto_name_precalc_idname = operators + '.auto_name_precalc'
 
     # Window ids
-    gt_precalc_window_idname = operators + '.precalc_window'
-    gt_resize_window_idname = operators + '.resize_window'
+    gt_rescale_window_idname = operators + '.rescale_window'
 
     # Menu ids
     gt_clip_menu_idname = _MT + 'clip_menu'
     gt_clear_tracking_menu_idname = _MT + 'clear_tracking_menu'
-
     gt_clear_tracking_menu_exec_idname = operators + '.clip_menu_exec'
 
     # Panel ids
@@ -166,9 +164,6 @@ class GTConfig:
     wireframe_color = (0.0, 1.0, 0.0, 0.4)
     mask_2d_color = (0.0, 1.0, 0.0, 0.4)
     mask_3d_color = (0.0, 0.0, 1.0, 0.4)
-
-    serial_prop_name = prefix + '_serial'
-    version_prop_name = prefix + '_version'
 
     prevent_view_rotation = True
     auto_render_size = True
