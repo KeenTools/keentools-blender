OT-3551 Scaling fixed via numpy dtype

keentools/geotracker/gtloader.py
@@ -34,7 +34,8 @@ from ..utils.coords import (image_space_to_frame,
                             compensate_view_scale,
                             frame_to_image_space,
                             camera_sensor_width,
-                            xy_to_xz_rotation_matrix_3x3)
+                            xy_to_xz_rotation_matrix_3x3,
+                            InvScaleMatrix)
 from ..utils.bpy_common import (bpy_render_frame,
                                 bpy_current_frame,
                                 get_scene_camera_shift,
@@ -462,15 +463,23 @@ class GTLoader:
 
         if normals:
             _log.output('normals')
-            # gt = GTLoader.kt_geotracker()
-            # geo = gt.geo()
-            # _log.output('wf.triangle_vertices')
-            # wf.triangle_vertices = pkt_module().utils.get_independent_triangles(geo) @ xy_to_xz_rotation_matrix_3x3()
-            # _log.output('wf.lit_edge_vertex_normals')
-            # wf.lit_edge_vertex_normals = pkt_module().utils.get_normals_for_lines(geo) @ xy_to_xz_rotation_matrix_3x3()
-            # _log.output('wf.lit_edge_vertices')
-            # wf.lit_edge_vertices = pkt_module().utils.get_lines(geo) @ xy_to_xz_rotation_matrix_3x3()
-            wf.init_vertex_normals(geotracker.geomobj)
+            # wf.init_vertex_normals(geotracker.geomobj)
+
+            gt = GTLoader.kt_geotracker()
+            geo = gt.geo()
+            scale = InvScaleMatrix(3, geotracker.geomobj.matrix_world.to_scale())
+            mat = xy_to_xz_rotation_matrix_3x3() @ np.array(scale, dtype=np.float32)
+            _log.output('wf.triangle_vertices')
+            wf.triangle_vertices = np.array(
+                pkt_module().utils.get_independent_triangles(geo),
+                dtype=np.float32) @ mat
+            _log.output('wf.lit_edge_vertex_normals')
+            wf.lit_edge_vertex_normals = np.array(
+                pkt_module().utils.get_normals_for_lines(geo),
+                dtype=np.float32) @ mat
+            _log.output('wf.lit_edge_vertices')
+            wf.lit_edge_vertices = np.array(pkt_module().utils.get_lines(geo),
+                                            dtype=np.float32) @ mat
 
         _log.output('wf.init_color_data')
         wf.init_color_data((*settings.wireframe_color,

keentools/utils/edges.py
@@ -582,8 +582,7 @@ class KTEdgeShaderLocal3D(KTEdgeShader3D):
                                             dtype=np.float32).transpose()
 
     def init_geom_data_from_mesh(self, obj: Any) -> None:
-        self.object_world_matrix = np.array(obj.matrix_world,
-                                            dtype=np.float32).transpose()
+        self.set_object_world_matrix(obj.matrix_world)
         mesh = evaluated_mesh(obj)
         self.vertices = get_mesh_verts(mesh)
         self.triangle_indices = get_triangulation_indices(mesh)
@@ -641,6 +640,7 @@ class KTLitEdgeShaderLocal3D(KTEdgeShaderLocal3D):
         self.lit_light3_pos: Vector = Vector((2, 0, 1))
         self.lit_camera_pos: Vector = Vector((0, 0, 0))
         self.lit_light_matrix: Matrix = Matrix.Identity(4)
+        self.fill_batch2: Optional[Any] = None
         super().__init__(target_class, mask_color)
 
     def set_lit_wireframe(self, state: bool) -> None:
@@ -721,6 +721,15 @@ class KTLitEdgeShaderLocal3D(KTEdgeShaderLocal3D):
                  'vertNormal': self.lit_edge_vertex_normals})
         else:
             _log.error(f'{self.__class__.__name__}.lit_shader: is empty')
+
+        if self.fill_shader is not None:
+            if self.lit_is_working():
+                _log.output(_log.color('red', 'Extra self.fill_shader'))
+                self.fill_batch = batch_for_shader(
+                    self.fill_shader, 'TRIS',
+                    {'pos': self.triangle_vertices})
+        else:
+            _log.error(f'{self.__class__.__name__}.fill_shader2: is empty')
 
     def draw_edges(self) -> None:
         if not self.lit_is_working():