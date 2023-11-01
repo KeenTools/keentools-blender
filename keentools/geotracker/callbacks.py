d7d26a5b5fb42d672438d278b24e19d21a30d425
Alexander Milovsky <alex@milovsky.ru>
2023-11-01 14:46:37 +0300

    Fix for focal length watcher

--- keentools/geotracker/callbacks.py
+++ keentools/geotracker/callbacks.py
@@ -66,8 +66,18 @@ _unbreak_rotation_is_needed_warning = \
     'We recommend applying \'Unbreak rotation\' operation to it'
 
 
-_camobj_lens_watcher_owner = object()
-_movie_clip_color_space_watcher_owner = object()
+class Owner:
+    def __del__(self):
+        _log.error('!!!!!!! OWNER OBJECT DESTROYED !!!!!!!')
+
+_old_focal_lens_mm: float = 50.0
+_camobj_lens_watcher_owner = Owner()
+_movie_clip_color_space_watcher_owner = Owner()
+
+
+def _set_old_focal_lens_mm(value: float):
+    global _old_focal_lens_mm
+    _old_focal_lens_mm = value
 
 
 def unsubscribe_watcher(owner: object) -> None:
@@ -82,9 +92,10 @@ def subscribe_camera_lens_watcher(camobj: Optional[Object]) -> None:
     if not camobj or not camobj.data:
         return
     subscribe_to = camobj.data.path_resolve('lens', False)
+    _set_old_focal_lens_mm(camobj.data.lens)
     bpy_msgbus_subscribe_rna(key=subscribe_to,
                              owner=_camobj_lens_watcher_owner,
-                             args=(camobj.data.lens,),
+                             args=(),
                              notify=lens_change_callback)
     _log.output('subscribe_camera_lens_watcher end')
 
@@ -111,18 +122,18 @@ def color_space_change_callback(old_name: str) -> None:
     update_movieclip(geotracker, None)
 
 
-def lens_change_callback(old_focal_length_mm: float) -> None:
+def lens_change_callback() -> None:
     _log.output(_log.color('magenta', 'lens_change_callback call'))
     settings = get_gt_settings()
     geotracker = settings.get_current_geotracker_item()
     if not settings.pinmode and not settings.is_calculating():
         _log.output('lens_change_callback stop 1')
-        subscribe_camera_lens_watcher(geotracker.camobj)
+        _set_old_focal_lens_mm(geotracker.camobj.data.lens)
         return
 
     if GTLoader.viewport().pins().move_pin_mode():
         _log.output('lens_change_callback stop 2')
-        subscribe_camera_lens_watcher(geotracker.camobj)
+        _set_old_focal_lens_mm(geotracker.camobj.data.lens)
         return
 
     if geotracker.focal_length_mode != 'STATIC_FOCAL_LENGTH':
@@ -134,7 +145,7 @@ def lens_change_callback(old_focal_length_mm: float) -> None:
     rw, rh = bpy_render_frame()
 
     old_focal_length_px = focal_mm_to_px(
-        old_focal_length_mm,
+        _old_focal_lens_mm,
         rw, rh, camera_sensor_width(geotracker.camobj))
     new_focal_length_px = focal_mm_to_px(
         camera_focal_length(geotracker.camobj),
@@ -151,7 +162,7 @@ def lens_change_callback(old_focal_length_mm: float) -> None:
                                                   new_focal_length_px, True,
                                                   current_frame)
         bpy_set_current_frame(current_frame)
-        subscribe_camera_lens_watcher(geotracker.camobj)
+        _set_old_focal_lens_mm(geotracker.camobj.data.lens)
         settings.stop_calculating()
         _log.output('FOCAL LENGTH CHANGED')
 
