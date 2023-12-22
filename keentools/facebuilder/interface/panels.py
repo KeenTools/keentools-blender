56288d8a2c130fee6a7efc529070820545101364
Alexander Milovsky <alex@milovsky.ru>
2023-12-22 12:45:11 +0300

    OT-3964 Options rollup (new version)

--- keentools/facebuilder/interface/panels.py
+++ keentools/facebuilder/interface/panels.py
@@ -206,10 +206,13 @@ def _draw_camera_hint(layout, headnum):
         col.label(text='in each view below')
 
 
-def _draw_expression_settings(layout, head):
+def _draw_expression_settings(layout, head, box_layout=True):
     if not head.should_use_emotions():
         return
-    box = layout.box()
+    if box_layout:
+        box = layout.box()
+    else:
+        box = layout
     col = box.column(align=True)
     col.prop(head, 'lock_blinking')
     col.prop(head, 'lock_neck_movement')
@@ -260,7 +263,7 @@ class FB_PT_HeaderPanel(Common, Panel):
     def _create_head_button(self, layout, active=True):
         settings = get_fb_settings()
         first_head = len(settings.heads) == 0
-        row = layout.row()
+        row = layout.row(align=True)
         row.enabled = active
         row.scale_y = 2.0 if first_head else Config.btn_scale_y
         row.operator(
@@ -344,9 +347,10 @@ class FB_PT_HeaderPanel(Common, Panel):
                 row.operator(FBConfig.fb_unhide_head_idname, icon='HIDE_OFF')
                 _start_pinmode_escaper(context)
 
-            self._draw_many_heads(layout, active=False)
-            # self._create_head_button(layout, active=False)
-            _draw_exit_pinmode(layout, scale=Config.btn_scale_y, depress=False)
+            col = layout.column(align=True)
+            self._draw_many_heads(col, active=False)
+            self._create_head_button(col, active=False)
+            # _draw_exit_pinmode(layout, scale=Config.btn_scale_y, depress=False)
 
         elif state == 'RECONSTRUCT':
             self._draw_reconstruct(layout)
@@ -361,8 +365,9 @@ class FB_PT_HeaderPanel(Common, Panel):
             return
 
         else:
-            self._draw_many_heads(layout)
-            self._create_head_button(layout)
+            col = layout.column(align=True)
+            self._draw_many_heads(col)
+            self._create_head_button(col)
             _exit_from_localview_button(layout, context)
             KTUpdater.call_updater('FaceBuilder')
             _fb_grace_timer.start()
@@ -394,17 +399,18 @@ class FB_PT_UpdatesInstallationPanel(KT_PT_UpdatesInstallationPanel):
     bl_category = Config.fb_tab_category
 
 
-class FB_PT_CameraPanel(AllVisible, Panel):
+class FB_PT_CameraPanel(AllVisibleClosed, Panel):
     bl_idname = FBConfig.fb_camera_panel_idname
-    bl_label = 'View'
+    bl_label = 'Options'
+    bl_parent_id = FBConfig.fb_views_panel_idname
 
     @classmethod
     def poll(cls, context):
         if not facebuilder_enabled():
             return False
-        settings = get_fb_settings()
-        if not settings.pinmode:
-            return False
+        # settings = get_fb_settings()
+        # if not settings.pinmode:
+        #     return False
         return _show_all_panels()
 
     def draw_header_preset(self, context):
@@ -421,17 +427,48 @@ class FB_PT_CameraPanel(AllVisible, Panel):
             return
 
         layout = self.layout
-        _draw_align_button(layout, scale=2.0, depress=True)
-        _draw_camera_info(layout)
+        col = layout.column(align=True)
+        col.label(text='Camera')
+        # _draw_align_button(layout, scale=2.0, depress=True)
+        _draw_camera_info(col)
+
+        head = settings.get_head(settings.current_headnum)
+        if not head:
+            return
+
+        col = layout.column(align=True)
+        col.label(text='Solver')
+        col.prop(head, 'use_emotions')
+        _draw_expression_settings(col, head, box_layout=False)
+
+        col = layout.column(align=True)
+        col.label(text='Rigidity')
+
+        row = col.row(align=True)
+        row.enabled = settings.pinmode
+        row.prop(settings, 'shape_rigidity', text='Shape')
+
+        row = col.row(align=True)
+        row.prop(settings, 'expression_rigidity', text='Expression')
+        row.enabled = head.should_use_emotions()
+
+        row = col.row(align=True)
+        row.prop(settings, 'blinking_rigidity', text='Eyelids')
+        row.enabled = not head.lock_blinking and head.should_use_emotions()
+
+        row =  col.row(align=True)
+        row.prop(settings, 'neck_movement_rigidity', text='Neck')
+        row.enabled = not head.lock_neck_movement and head.should_use_emotions()
+
         # _draw_exit_pinmode(layout)
-        if settings.pinmode and \
-                context.space_data.region_3d.view_perspective == 'CAMERA':
-            _draw_pins_panel(layout)
+        # if settings.pinmode and \
+        #         context.space_data.region_3d.view_perspective == 'CAMERA':
+        #     _draw_pins_panel(layout)
 
 
 class FB_PT_ViewsPanel(AllVisible, Panel):
     bl_idname = FBConfig.fb_views_panel_idname
-    bl_label = 'Images'
+    bl_label = 'Views'
 
     def draw_header_preset(self, context):
         layout = self.layout
@@ -461,6 +498,7 @@ class FB_PT_ViewsPanel(AllVisible, Panel):
             view_icon = 'PINNED' if camera.has_pins() else 'HIDE_OFF'
 
             if settings.current_camnum == i and settings.pinmode:
+                view_icon = 'LOOP_BACK'
                 row.operator(FBConfig.fb_select_current_camera_idname,
                              text=camera.get_image_name(), icon=view_icon,
                              depress=True)
@@ -486,7 +524,7 @@ class FB_PT_ViewsPanel(AllVisible, Panel):
         col = layout.column(align=True)
         col.scale_y = scale
         op = col.operator(FBConfig.fb_multiple_filebrowser_exec_idname,
-                          text='Add Images', icon=icon)
+                          text='Add Images' if icon != 'ADD' else '', icon=icon)
         op.headnum = headnum
 
     def draw(self, context):
@@ -506,6 +544,13 @@ class FB_PT_ViewsPanel(AllVisible, Panel):
             _draw_update_blendshapes_panel(layout)
 
         self._draw_camera_list(headnum, layout)
+
+        if settings.pinmode:
+            _draw_align_button(layout, scale=2.0, depress=True)
+
+            op = layout.operator(FBConfig.fb_unmorph_idname, text='Reset View')
+            op.headnum = headnum
+            op.camnum = settings.current_camnum
 
         # _draw_exit_pinmode(layout)
 
@@ -542,32 +587,32 @@ class FB_PT_Model(AllVisibleClosed, Panel):
 
         head = settings.get_head(headnum)
 
-        col = layout.column()
-        col.scale_y = Config.btn_scale_y
-        op = col.operator(FBConfig.fb_unmorph_idname, text='Reset')
-        op.headnum = headnum
-        op.camnum = settings.current_camnum
+        # col = layout.column()
+        # col.scale_y = Config.btn_scale_y
+        # op = col.operator(FBConfig.fb_unmorph_idname, text='Reset')
+        # op.headnum = headnum
+        # op.camnum = settings.current_camnum
+        #
+        # layout.prop(head, 'use_emotions')
+        # _draw_expression_settings(layout, head)
+        #
+        # col = layout.column(align=True)
+        # col.enabled = settings.pinmode
+        # col.prop(settings, 'shape_rigidity')
+        #
+        # row = col.row(align=True)
+        # row.prop(settings, 'expression_rigidity')
+        # row.enabled = head.should_use_emotions()
+        #
+        # row = col.row(align=True)
+        # row.prop(settings, 'blinking_rigidity')
+        # row.enabled = not head.lock_blinking and head.should_use_emotions()
+        #
+        # row =  col.row(align=True)
+        # row.prop(settings, 'neck_movement_rigidity')
+        # row.enabled = not head.lock_neck_movement and head.should_use_emotions()
 
-        layout.prop(head, 'use_emotions')
-        _draw_expression_settings(layout, head)
-
-        col = layout.column(align=True)
-        col.enabled = settings.pinmode
-        col.prop(settings, 'shape_rigidity')
-
-        row = col.row(align=True)
-        row.prop(settings, 'expression_rigidity')
-        row.enabled = head.should_use_emotions()
-
-        row = col.row(align=True)
-        row.prop(settings, 'blinking_rigidity')
-        row.enabled = not head.lock_blinking and head.should_use_emotions()
-
-        row =  col.row(align=True)
-        row.prop(settings, 'neck_movement_rigidity')
-        row.enabled = not head.lock_neck_movement and head.should_use_emotions()
-
-        layout.prop(head, 'model_scale')
+        # layout.prop(head, 'model_scale')
 
         row = layout.split(factor=0.35)
         row.label(text='Topology')
@@ -589,6 +634,15 @@ class FB_PT_Model(AllVisibleClosed, Panel):
             if i % 2 == 0:
                 row = col.row(align=True)
             row.prop(head, 'masks', index=i, text=names[i])
+
+        layout.prop(head, 'model_scale')
+
+        col = layout.column()
+        col.scale_y = Config.btn_scale_y
+        col.alert = True
+        op = col.operator(FBConfig.fb_unmorph_idname, text='Reset Head')
+        op.headnum = headnum
+        op.camnum = settings.current_camnum
 
 
 class FB_PT_TexturePanel(AllVisibleClosed, Panel):
@@ -689,7 +743,7 @@ class FB_PT_AppearancePanel(AllVisibleClosed, Panel):
             FBConfig.fb_default_wireframe_settings_idname,
             text='', icon='LOOP_BACK', emboss=False, depress=False)
 
-        split = col.split(factor=0.4, align=True)
+        split = col.split(factor=0.375, align=True)
         split2 = split.split(factor=0.34, align=True)
         split2.prop(settings, 'wireframe_color', text='')
         split3 = split2.split(factor=0.5, align=True)
