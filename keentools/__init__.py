feefba246f78393022a3f822136b9c9ddeff57ab
Alexander Milovsky <alex@milovsky.ru>
2024-06-28 14:21:18 +0300

    OT-4202 All version changes were numbered

--- keentools/__init__.py
+++ keentools/__init__.py
@@ -17,8 +17,8 @@
 # ##### END GPL LICENSE BLOCK #####
 
 bl_info = {
-    "name": "KeenTools: FaceBuilder, FaceTracker, GeoTracker 2024.1.0",  # (1/5)
-    "version": (2024, 1, 0),  # 2024.1.0 (2/5)
+    "name": "KeenTools: FaceBuilder, FaceTracker, GeoTracker 2024.1.0",  # (1/6)
+    "version": (2024, 1, 0),  # 2024.1.0 (2/6)
     "author": "KeenTools",
     "description": "FaceBuilder: Create Heads. FaceTracker: Track Heads. GeoTracker: Track Objects",
     "blender": (2, 80, 0),
@@ -74,7 +74,7 @@ def _is_python_64bit():
 
 
 def _is_config_latest():
-    return Config.addon_version == '2024.1.0'  # (3/5)
+    return Config.addon_version == '2024.1.0'  # (3/6)
 
 
 def _is_blender_too_old():
