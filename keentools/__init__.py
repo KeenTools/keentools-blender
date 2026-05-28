2f74142bf2c824a9919b5cf27cadda0f18b2b8a3
Alexander Milovsky <alex@keentools.io>
2026-05-28 18:25:56 +0300

    Version changed to 2026.2.1

--- keentools/__init__.py
+++ keentools/__init__.py
@@ -17,8 +17,8 @@
 # ##### END GPL LICENSE BLOCK #####
 
 bl_info = {
-    "name": "KeenTools: FaceBuilder, FaceTracker, GeoTracker 2026.2.0",  # (1/6)
-    "version": (2026, 2, 0),  # 2026.2.0 (2/6)
+    "name": "KeenTools: FaceBuilder, FaceTracker, GeoTracker 2026.2.1",  # (1/6)
+    "version": (2026, 2, 1),  # 2026.2.1 (2/6)
     "author": "KeenTools",
     "description": "FaceBuilder: Create Heads. FaceTracker: Track Heads. GeoTracker: Track Objects",
     "blender": (2, 80, 0),
